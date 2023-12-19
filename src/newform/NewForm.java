26fd8cf45179946ddc48847c5ac581e1832e220e
theo117 <tednel3@gmail.com>
2023-12-19 08:07:38 +0200

    Fish and Chips app
    Fish and Chips app that calculates the amount to be paid and the change based on the products choosen

---
+++ src/newform/NewForm.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package newform;
+
+/**
+ *
+ * @author Latitude 7480
+ */
+public class NewForm {
+
+    
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    NewJFrame frmMyFrame = new NewJFrame();
+    frmMyFrame.show();
+    
+    }
+    
+    
+}
